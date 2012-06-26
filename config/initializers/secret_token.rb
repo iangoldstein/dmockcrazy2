8677e7f23bcc9d9f74cb555ae899d7bf1b956302
Ian G. Goldstein <ian08@goldstein.org>
2012-06-26 00:59:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmockcrazy2::Application.config.secret_token = '148d37cce107d1f9b6f817d774e4aa4a2b75dcde805d6235f5c129aa2b8f4f8bc0233b75424134c8b9f5f55d9d5327785dbc1d5d7475110ceaffdfeccfc6abb2'
